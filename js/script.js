773db0314dcd28c0f89abef092a2c906e338daae
chintriago <35647407+chintriago@users.noreply.github.com>
2022-04-08 12:58:21 -0700

    updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("test successfull");
+const searchInput = document.getElementById("gps-search");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let searchInputVal = searchInput.value;
+    // let newArr = [];
+    // for (let i = 0; i < searchInputVal.length; i++) {
+    //     let result = searchInputVal[i].replace("/", ",");
+    //     newArr[i] = result;
+    // }
+    // alert("you have submitted: " + newArr);
+    let result = searchInputVal.replace("/", ",")
+    alert("you have submitted: " + result);
+});
